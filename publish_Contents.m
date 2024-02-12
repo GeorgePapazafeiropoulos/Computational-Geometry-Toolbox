5628cce2fa3228cd205e0c0bb3f22ffdda8b488c
George Papazafeiropoulos <gpapazafeiropoulos@yahoo.gr>
2024-02-12 11:41:09 +0200

    Added code and examples

---
+++ publish_Contents.m
@@ -0,0 +1,117 @@
+%% Publish all examples included in the computational geometry toolbox
+% To reproduce all numerical results, figures, etc. contained in this
+% toolbox, run this file.
+
+publish('example_Bounding_box_2D.m')
+close all
+publish('example_Bounding_box_3D.m')
+close all
+publish('example_Bounding_box_4D.m')
+close all
+publish('example_Bounding_box_5D.m')
+close all
+publish('example_Concave_hull_grid_2D.m')
+close all
+publish('example_Concave_hull_rand_2D.m')
+close all
+publish('example_Concave_mesh_grid_2D.m')
+close all
+publish('example_Concave_mesh_rand_2D.m')
+close all
+publish('example_Convex_hull_circle_2D.m')
+close all
+publish('example_Convex_hull_large_point_set.m')
+close all
+publish('example_Convex_hull_rand_2D.m')
+close all
+publish('example_Convex_hull_rand_3D.m')
+close all
+publish('example_Convex_hull_sphere_3D.m')
+close all
+publish('example_Convex_hull_verification_2D.m')
+close all
+publish('example_Convex_hull_verification_3D.m')
+close all
+publish('example_Convex_hull_verification_4D.m')
+close all
+publish('example_Convex_hull_verification_5D.m')
+close all
+publish('example_Divide_hypercube_5_simplices_3D.m')
+close all
+publish('example_Divide_hypercube_6_simplices_3D.m')
+close all
+publish('example_Divide_hypercube_simplices_4D.m')
+close all
+publish('example_Divide_hypercube_simplices_5D.m')
+close all
+publish('example_Facet_enumeration_3D.m')
+close all
+publish('example_Intersection_2D.m')
+close all
+publish('example_Intersection_3D.m')
+close all
+publish('example_Intersection_4D.m')
+close all
+publish('example_Mesh_cube_3D.m')
+close all
+publish('example_Mesh_rand_2D.m')
+close all
+publish('example_Mesh_rand_3D.m')
+close all
+publish('example_Mesh_square_2D.m')
+close all
+publish('example_Mesh_verification_2D.m')
+close all
+publish('example_Mesh_verification_3D.m')
+close all
+publish('example_Mesh_verification_4D.m')
+close all
+publish('example_Mesh_verification_5D.m')
+close all
+publish('example_Point_in_polygon_2D.m')
+close all
+publish('example_Point_in_polygon_3D.m')
+close all
+publish('example_Point_in_polygon_4D.m')
+close all
+publish('example_Point_in_polygon_5D.m')
+close all
+publish('example_Scattered_data_interpolation_2D.m')
+close all
+publish('example_Scattered_data_interpolation_3D.m')
+close all
+publish('example_Scattered_data_interpolation_4D.m')
+close all
+publish('example_Theory.m')
+close all
+publish('example_Upper_Bound_Theorem_2D.m')
+close all
+publish('example_Upper_Bound_Theorem_3D.m')
+close all
+publish('example_Upper_Bound_Theorem_4D.m')
+close all
+publish('example_Upper_Bound_Theorem_5D.m')
+close all
+publish('example_Vertex_enumeration_3D.m')
+close all
+publish('example_Voronoi_cube_3D.m')
+close all
+publish('example_Voronoi_rand_3D.m')
+close all
+publish('example_Voronoi_square_2D.m')
+close all
+publish('example_Voronoi_rand_2D.m')
+close all
+
+%% Contact author
+%
+%  (c) 2024 by George Papazafeiropoulos
+%  First Lieutenant, Infrastructure Engineer, Hellenic Air Force
+%  Civil Engineer, M.Sc., Ph.D. candidate, NTUA
+%
+% Email: gpapazafeiropoulos@yahoo.gr
+%
+% Website: http://users.ntua.gr/gpapazaf/
+%
+
+
